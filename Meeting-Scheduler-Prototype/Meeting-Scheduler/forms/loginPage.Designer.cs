de6df8216737649d5a37997c022220fde18a7790
Jess <jessica.s.read@outlook.com>
2020-12-04 15:19:01 +0000

    scheduled meetings

--- Meeting-Scheduler-Prototype/Meeting-Scheduler/forms/loginPage.Designer.cs
+++ Meeting-Scheduler-Prototype/Meeting-Scheduler/forms/loginPage.Designer.cs
@@ -60,6 +60,7 @@ namespace Meeting_Scheduler
             this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
